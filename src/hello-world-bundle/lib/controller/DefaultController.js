f06e88676f8d68838cf8c96d64a383c2c52f9517
Marc Roulias <marc@marcroulias.com>
2017-08-01 02:32:12 -0700

    added additional sample route and git ignoring parameters.yml

--- src/hello-world-bundle/lib/controller/DefaultController.js
+++ src/hello-world-bundle/lib/controller/DefaultController.js
@@ -5,14 +5,14 @@ const os = require('os');
 /**
  * The DefaultController defines the default actions and routes
  *
- * @Route("/")
+ * @Route("/")  <-- this defines the prefix for all routes in this controller
  */
 module.exports = class DefaultController extends Controller {
 
     /**
-     * Show the main application index page
+     * Show the application's index page
      *
-     * @Route("/", name="default.index", methods=["GET"])
+     * @Route("/", name="default.index", methods=["GET"])  <-- this defines the route info for this action
      */
     index(req, res) {
 
@@ -29,6 +29,33 @@ module.exports = class DefaultController extends Controller {
                 platform: os.platform(),
                 release: os.release()
             }
+        });
+
+    }
+
+    /**
+     * Here is an action that uses URL parameters and also demonstrates using a promise
+     *
+     * Go to http://localhost:3000/hello/world
+     *
+     * or:
+     *
+     * http://localhost:3000/hello/error to trigger an error
+     *
+     * @Route("/hello/:name", methods=["GET"])
+     */
+    hello(req, res) {
+
+        // you can also return promises
+        return new Promise((resolve, reject) => {
+
+            if (req.params.name === 'error') {
+                reject(this.buildErrorResponse({ error: 'this is an error', code: 403}, 403));
+            }
+
+            resolve({
+                message: 'Hello ' + req.params.name
+            });
         });
 
     }
